0303eb90c395e89f6e87a4f856a49a00cb676b33
Lukas Råberg <lukas.raberg@inuse.se>
2018-04-17 20:30:01 +0200

    Extended click areas
    Widely extended click areas from just the title tag to the whole div,
    that´s now wrapped with an a-tag

--- articles.php
+++ articles.php
@@ -1,92 +1,100 @@
 <div class="featured-wrapper">
 	<?php query_posts('cat=6&posts_per_page=1'); ?>
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-		<div class="featured featured-fill featured-first"> 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php the_post_thumbnail('large'); ?>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			<div class="featured featured-fill featured-first"> 
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php the_post_thumbnail('large'); ?>
 			
-				<h2 class="featured-category">
-					<?php the_category(); ?>
-				</h2>
-				<div class="selected-title-thin"></div>
-				<h3 class="featured-title">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h3>
-				<div class="selected-title-thin"></div>
-				<span class="featured-author">
-					 <?php the_author(); ?>
-				</span>
-			</article>
-		</div>
+					<h2 class="featured-category">
+						<?php echo get_cat_name(6); ?>
+					</h2>
+					<div class="selected-title-thin"></div>
+					<h3 class="featured-title">
+						<?php the_title(); ?>
+					</h3>
+					<div class="selected-title-thin"></div>
+					<span class="featured-author">
+						 <?php the_author(); ?>
+					</span>
+				</article>
+			</div>
+		</a>
 	<?php endwhile; ?>
 	<?php else: ?>
 	<?php endif; ?>
 	
 	<?php query_posts('cat=2&posts_per_page=1'); ?>
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-		<div class="featured featured-fill featured-second"> 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php the_post_thumbnail('large'); ?>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			<div class="featured featured-fill featured-second"> 
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php the_post_thumbnail('large'); ?>
 			
-				<h2 class="featured-category">
-					<?php the_category(); ?>
-				</h2>
-				<div class="selected-title-thin"></div>
-				<h3 class="featured-title">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h3>
-				<div class="selected-title-thin"></div>
-				<span class="featured-author">
-					 <?php the_author(); ?>
-				</span>
-			</article>
-		</div>
+					<h2 class="featured-category">
+						<?php echo get_cat_name(2); ?>
+					</h2>
+					<div class="selected-title-thin"></div>
+					<h3 class="featured-title">
+						<?php the_title(); ?>
+					</h3>
+					<div class="selected-title-thin"></div>
+					<span class="featured-author">
+						 <?php the_author(); ?>
+					</span>
+				</article>
+			</div>
+		</a>
 	<?php endwhile; ?>
 	<?php else: ?>
 	<?php endif; ?>
 	
 	<?php query_posts('cat=4&posts_per_page=1'); ?>
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-		<div class="featured featured-fill featured-first"> 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php the_post_thumbnail('large'); ?>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			<div class="featured featured-fill featured-first"> 
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php the_post_thumbnail('large'); ?>
 			
-				<h2 class="featured-category">
-					<?php the_category(); ?>
-				</h2>
-				<div class="selected-title-thin"></div>
-				<h3 class="featured-title">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h3>
-				<div class="selected-title-thin"></div>
-				<span class="featured-author">
-					 <?php the_author(); ?>
-				</span>
-			</article>
-		</div>
+					<h2 class="featured-category">
+						<?php echo get_cat_name(4); ?>
+					</h2>
+					<div class="selected-title-thin"></div>
+					<h3 class="featured-title">
+						<?php the_title(); ?>
+					</h3>
+					<div class="selected-title-thin"></div>
+					<span class="featured-author">
+						 <?php the_author(); ?>
+					</span>
+				</article>
+			</div>
+		</a>
 	<?php endwhile; ?>
 	<?php else: ?>
 	<?php endif; ?>
 	
 	<?php query_posts('cat=3&posts_per_page=1'); ?>
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-		<div class="featured featured-fill featured-second"> 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php the_post_thumbnail('large'); ?>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			<div class="featured featured-fill featured-second"> 
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php the_post_thumbnail('large'); ?>
 			
-				<h2 class="featured-category">
-					<?php the_category(); ?>
-				</h2>
-				<div class="selected-title-thin"></div>
-				<h3 class="featured-title">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h3>
-				<div class="selected-title-thin"></div>
-				<span class="featured-author">
-					 <?php the_author(); ?>
-				</span>
-			</article>
-		</div>
+					<h2 class="featured-category">
+						<?php echo get_cat_name(3); ?>
+					</h2>
+					<div class="selected-title-thin"></div>
+					<h3 class="featured-title">
+						<?php the_title(); ?>
+					</h3>
+					<div class="selected-title-thin"></div>
+					<span class="featured-author">
+						 <?php the_author(); ?>
+					</span>
+				</article>
+			</div>
+		</a>
 	<?php endwhile; ?>
 	<?php else: ?>
 	<?php endif; ?>
--- eyeson.php
+++ eyeson.php
@@ -1,21 +1,21 @@
 <div class="selected" id="eyeson">
 <?php query_posts('cat=7&posts_per_page=1'); ?>
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+	<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+		<div class="selected-img"><?php the_post_thumbnail('selected'); ?></div>
 
-	<div class="selected-img"><?php the_post_thumbnail('selected'); ?></div>
-
-	<div class="selected-puff">		
-			<h3 class="featured-category">
-				<?php echo get_cat_name(7); ?>
+		<div class="selected-puff">		
+				<h3 class="featured-category">
+					<?php echo get_cat_name(7); ?>
+				</h3>
+			<h3 class="selected-title">
+				<div class="selected-title-thin"></div>
+					<?php the_title(); ?>
+				<div class="selected-title-thin"></div>
 			</h3>
-		<h3 class="selected-title">
-			<div class="selected-title-thin"></div>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			<div class="selected-title-thin"></div>
-		</h3>
-		<div class="authors"><?php echo get_author_name(); ?></div>
-	</div>
-
+			<div class="authors"><?php echo get_author_name(); ?></div>
+		</div>
+	</a>
 <?php endwhile; ?>
 <?php else: ?>
 <?php endif; ?>
