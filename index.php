231e4fc33d472fe5467e9061de895a8080074cfe
Lukas Råberg <lukas.raberg@inuse.se>
2018-05-07 20:57:43 +0200

    Making the manifest/seo text editable from wp admin

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 <?php get_template_part('eyeson')?>
 <?php get_template_part('articles'); ?>
 <?php get_template_part('authors'); ?>
-<?php get_template_part('introimg')?>
 <?php get_template_part('manifest'); ?>
-
 <?php get_template_part('map'); ?>
 
 <?php get_footer(); ?> 
--- manifest.php
+++ manifest.php
@@ -1,31 +1,15 @@
+<div class="hero"></div>
 <div class="clearboth-alt"></div>
+<?php query_posts('cat=25&posts_per_page=1'); ?>
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 <div class="manifest-wrapper">
-	<h1>Lifestyle, drinkar, mat och hemma hos</h1>
+	<h1><?php the_title(); ?></h1>
 
-		<p class="intro">Apples of my Eye är ett onlinemagasin med utgångspunkt Österlen med omnejd. Vår vision är att genom ett livsstilskoncept guida läsare med inspirerande reportage och bilder och därmed stärka och förändra områdets identitet. Vi vill visa det Österlen som vi tilltalas av.</p>
+	<p class="intro"><?php the_excerpt(); ?></p>
 	<div class="manifest">
-		<div class="mf">
-			<h2>Utgångspunkt Österlen och Ystad</h2>
-				<p class="inline">Apples of my Eye har sin utgångspunkt på Österlen och i Ystad. Vi som är grundare till magasinet har alla valt att bosätta oss här eller besöker området så ofta vi kan. Tillsammans plockar vi fram godbitarna som du inte vill missa! Vi vill visualisera hur livet i en mindre kommun kan vara utan att behöva göra avkall på sin stilistiska förmåga, kreativitet eller kunskap.
-
-		Vi kommer också blicka utanför Österlen och ta er med på “Getaways”, här följer ni med oss till bland annat Toscana, Miami, Los Angeles och andra spännande platser.</p>
-		</div>
-
-		<div class="mf">
-			<h2>Utvalda platser där du kan äta riktigt bra mat och lunch.</h2>
-				<p class="inline">För att underlätta för våra besökare har vi valt att pinna ut våra utvalda besöksmål på en karta. Från den kan besökare lägga upp en rutt för sin utflykt. Kanske startar man dagen med en lunch i Ystad för att sedan ge sig iväg på Österlentur och avsluta kvällen med en bit mat i Simrishamn.</p>
-		</div>
-		
-		<div class="mf">
-			<h2>Lifestyle på Österlen, hemma hos och close up</h2>
-				<p class="inline">Apples of my Eye vill lyfta fram entreprenörer och visionärer som finns på Österlen. Detta presenteras som lifestyleartiklar där vi får möjligheten att läsa om dessa modiga människors spännande berättelser. 
-
-		Vårt magasin kommer också att visa snygga “hemma hos” reportage. Detta finner våra läsare under konceptet Lifestyle. Här bjuder vi på close ups med härliga människor. Vi kommer besöka både sommarhus och permanenta boenden.</p>
-		</div>
-
-		<div class="mf">
-			<h2>Gin, romdrinkar och andra innovativa cocktails, olika varianter av mojitos och bubbliga drinkar med Prosecco.</h2>
-				<p class="inline">Varje fredag kommer en ny fredagsdrink från oss på Apples of my Eye. Vi tycker att det är ett härligt sätt att välkomna helgen på. Vi presenterar en blandning av olika typer av cocktails, som somriga mojitos med rom, drinkar med gin i spännande smakkombinationer och bubbliga proseccodrinkar. Titta gärna in varje fredag för att få inspiration till hur du kan fira in helgen med en fredagsdrink.</p>
-		</div>	
+		<?php the_content();?>	
 	</div>
+	<?php endwhile; ?>
+	<?php else: ?>
+	<?php endif; ?>
 </div>
