84e2b318d9fd64f9e7b6df34c98f072e7ed94070
Lukas Råberg <lukas.raberg@inuse.se>
2018-05-07 20:42:36 +0200

    Blog intro fixes

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <?php get_template_part('eyeson')?>
+<?php get_template_part('articles'); ?>
 <?php get_template_part('authors'); ?>
 <?php get_template_part('introimg')?>
 <?php get_template_part('manifest'); ?>
